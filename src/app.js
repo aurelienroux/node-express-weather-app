8699d5043a8699cdb655903e045eaeecf9a201c4
Aurélien Roux <aureroux13@gmail.com>
2020-11-20 16:20:18 -0500

    hbs dynamic views

--- src/app.js
+++ src/app.js
@@ -4,7 +4,26 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '..', '/public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+  res.render('index', { title: 'my hbs title' })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Aure'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpText: 'some help text'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
@@ -14,6 +33,4 @@ app.get('/weather', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000') // eslint-disable-line
-})
+app.listen(3000, () => console.log('Server is up on port 3000')) // eslint-disable-line
