4386466b726d7ec65a0115ca46c313a70539fe5e
Aurélien Roux <aureroux13@gmail.com>
2020-11-23 11:50:38 -0500

    58 search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
-fetch('http://localhost:3000/weather?address=frejus')
-  .then((response) => response.json())
-  .then((data) => console.log('data', data))
-  .catch((err) => console.log('err', err))
+const weatherForm = document.querySelector('form') //eslint-disable-line
+const searchInput = document.querySelector('input') //eslint-disable-line
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = searchInput.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`) //eslint-disable-line
+    .then((response) => response.json())
+    .then((data) => console.log('data', data)) //eslint-disable-line
+    .catch((err) => console.log('err', err)) //eslint-disable-line
+})
