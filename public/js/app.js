ff2f58bac9b9fb9c21736267f02716427e80c651
Aurélien Roux <aureroux13@gmail.com>
2020-11-23 11:26:06 -0500

    client fetch request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,4 @@
-console.log('client side js')
+fetch('http://localhost:3000/weather?address=frejus')
+  .then((response) => response.json())
+  .then((data) => console.log('data', data))
+  .catch((err) => console.log('err', err))
